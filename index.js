dcc90ff1efadfc97ef7d0b9fbd5c7e187279cf4d
marjan <74914575+Marjan-Binta-Aziz@users.noreply.github.com>
2022-04-30 22:21:42 +0600

    'heroku'

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
- 
+
+//-----------------------------------//
+// ========database info ====== =//
+// DB_NAME= warehouseManagement
+//DB_PASS= kbIeHpmEh5u50Ia6
+//BDGRL1ZGjH2l5P8t
+//-----------------------------------//
+const uri = "mongodb+srv://DB_NAME:DB_PASS@cluster0.wntwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+            await client.connect();
+            const itemCollection = client.db('dbWarehouse').collection('item');
+    }
+    finally{}
+}
+
+
+
 app.get('/' , (req, res) => {
     res.send('warehouse-management-server')
 })
